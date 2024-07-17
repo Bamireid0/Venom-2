7218f786aedce2a411f636e690cd018d4dc79b39
Bamireid0 <166797021+Bamireid0@users.noreply.github.com>
2024-07-17 18:31:45 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,19 +4,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916909137213"
-global.ownername = "David Cyril"
-global.ytname = "David Cyril"
-global.socialm = "GitHub: DeeCeeXxx"
+global.ownernomer = "2347015983330"
+global.ownername = "Lucian"
+global.ytname = "Bamireid"
+global.socialm = "GitHub: Bamireid0"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
-global.ownername = 'David Cyril' //owner name
-global.botname = '🕷Venom-2🕷' //name of the bot
+global.ownernumber = '2347015983330,2348102925093'  //creator number
+global.ownername = 'Lucian' //owner name
+global.botname = 'Morningstar' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'David_Cyril\n\nContact: +2349066528353'
+global.author = 'Lucian\n\nContact: +2347015983330'
 
 //console view/theme
 global.themeemoji = '🪀'
